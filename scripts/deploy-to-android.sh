5ebf4df1f7e9a5ec675831d1d5c47f2aa932a4e1
Uzzi Emuchay <Uzzije2000@yahoo.co.uk>
2017-03-06 14:59:18 -0600

    sstuff

--- scripts/deploy-to-android.sh
+++ scripts/deploy-to-android.sh
@@ -6,7 +6,7 @@ mv /Users/Administrator/DEVELOPMENT/pondeye/Pondeye.apk ~/Trash
 mv /Users/Administrator/DEVELOPMENT/pondeye/platforms/android/build/outputs/apk/android-release-unsigned.apk ~/Trash
 cordova build --release android
 jarsigner -verbose -sigalg SHA1withRSA -tsa http://tsa.safecreative.org/ -digestalg \
-SHA1 -keystore /Users/Administrator/Documents/OneDrive/Elude Things/Pondeye/my-release-key.keystore \
+SHA1 -keystore my-release-key.keystore \
 /Users/Administrator/DEVELOPMENT/pondeye/platforms/android/build/outputs/apk/android-release-unsigned.apk pondeye_key
 ~/Library/Android/sdk/build-tools/25.0.2/zipalign -v 4 \
 /Users/Administrator/DEVELOPMENT/pondeye/platforms/android/build/outputs/apk/android-release-unsigned.apk Pondeye.apk
